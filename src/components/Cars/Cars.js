db7d6cb67e2bc7db4d40cc2981885e7fad3dc263
dmknpd <soupuabt@gmail.com>
2024-05-13 11:15:26 +0300

    News component added and carsList edited

--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -1,12 +1,14 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import CarsList from "./CarsList";
+import News from "../News/News";
 
 const Cars = () => {
   return (
     <div className="cars-page">
       <Header />
       <CarsList />
+      <News />
       <Footer />
     </div>
   );
